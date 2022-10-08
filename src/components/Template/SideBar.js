496b93934831452cd063022c129560d894fcc754
Ferdinand Estoque <festoqufx@gmail.com>
2022-10-08 14:51:00 +0800

    upload1

---
+++ src/components/Template/SideBar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ContactIcons from '../Contact/ContactIcons';
+
+const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+
+const SideBar = () => (
+  <section id="sidebar">
+    <section id="intro">
+      <Link to="/" className="logo">
+        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+      </Link>
+      <header>
+        <h2>Ferdinand Estoque</h2>
+        <p><a href="mailto:ferdinand.estoque@yahoo.com">ferdinand.estoque@yahoo.com</a></p>
+      </header>
+    </section>
+
+    <section className="blurb">
+      <h2>About</h2>
+      <p>Hi, I&apos;m Raven. I like building things.
+        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
+        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
+        at <a href="https://matroid.com">Matroid</a>
+        , <a href="https://planet.com">Planet</a>
+        , <a href="https://planetaryresources.com">Planetary Resources</a>
+        , <a href="https://facebook.com">Facebook</a>
+        , and <a href="https://seds.org">SEDS</a>.
+      </p>
+      <ul className="actions">
+        <li>
+          {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+        </li>
+      </ul>
+    </section>
+
+    <section id="footer">
+      <ContactIcons />
+      <p className="copyright">&copy; Ferdinand Estoque <Link to="/">ferdinandestoque.com</Link>.</p>
+    </section>
+  </section>
+);
+
+export default SideBar;
